b7df6fbfbfee9d764fd6bd84b2bb5dec5bf4df1d
Abbu Turab <syedabbuturab786@gmail.com>
2025-04-10 00:13:23 -0500

    Added typeDef and query to fetch movie details with avg ratings

--- src/resolvers/movieResolvers.js
+++ src/resolvers/movieResolvers.js
@@ -6,7 +6,7 @@ const resolvers = {
     Query: {
         movies: (_, { page = 1 }) => {
             const offset = (page - 1) * MOVIES_PER_PAGE;
-            const sql = `SELECT imdbId, title, genres, releaseDate, budget
+            const sql = `SELECT movieId, imdbId, title, genres, releaseDate, budget
             FROM movies LIMIT ? OFFSET ? `;
 
             return new Promise((resolve, reject) => {
@@ -16,6 +16,7 @@ const resolvers = {
 
                     const formatted = rows.map((row) => ({
                         imbdId: row.imbdId,
+                        movieId: row.movieId,
                         title: row.title,
                         genres: row.genres,
                         releaseDate: row.releaseDate,
@@ -24,6 +25,10 @@ const resolvers = {
                     resolve(formatted);
                 })
             })
+        },
+        movie: (_, { imdbId }) => {
+
+            const sql = `SELECT * FROM movies WHERE imdbId = ?`
         }
     }
 }
--- src/schema.js
+++ src/schema.js
@@ -2,13 +2,27 @@ const typeDefs = `#graphql
 
     type Movie {
         imdbId: ID!
+        movieId: ID!
         title: String!
         genres: [String!]!
         releaseDate: String!
         budget: String!
     }
+    type MovieDetials {
+        imdbId: ID!
+        title: String!
+        description: String
+        runtime:Int!
+        averageRating: Float!
+        genres: [String!]!
+        releaseDate: String!
+        originalLanguage: String!
+        budget: String!
+        productionCompanies:[String!]!
+    }
     type Query {
         movies(page: Int!): [Movie]
+        movie(imdbId: ID!): MovieDetials
     }
 
 `
